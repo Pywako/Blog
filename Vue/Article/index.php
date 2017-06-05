33b79667df100e61681c26fe170f81e81de0c0aa
pywako <wongaliceyy@gmail.com>
2017-06-05 13:46:38 +0200

    Ajout des controleur et vue pour l'ajout de chapitre

---
+++ Vue/Article/index.php
@@ -0,0 +1,22 @@
+<?php
+/**
+Page d'écriture du chapitre
+ */
+
+$this->titre = "Billet pour l'Alaska - Ecriture" ?>
+<header>
+    <h2>Ecriture d'un chapitre</h2>
+</header>
+
+  <form method="post" action="article/ecrire">
+      <p>
+          <label for="titre">Titre du chapitre : </label>
+          <input type="text" name="titre" id="titre"> <br>
+          <label for="publication">état publication du chapitre : </label>
+          <input type="radio" name="publication" value="à publier">
+          <input type="radio" name="publication" value="au brouillon" checked><br>
+      </p>
+      <textarea id="chapitre" name="contenu">Hello, World!</textarea>
+      <input type="hidden" name="id" value="<?= $article['id'] ?>">
+      <input type="submit" value = "envoyer">
+  </form>
