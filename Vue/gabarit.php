d88ce75c76698191bcecfebfac24ec520cfbce9d
pywako <wongaliceyy@gmail.com>
2017-06-11 23:08:17 +0200

    Ajout chargement bootstrap et fonctionnalité tinymce

--- Vue/gabarit.php
+++ Vue/gabarit.php
@@ -12,11 +12,26 @@
         <base href="<?= $racineBlog ?>">
         <link rel="stylesheet" href="Contenu/style.css">
         <script src="../tinymce/js/tinymce/tinymce.min.js"></script>
-        <script type="text/javascript">
-            tinymce.init({
-                selector: '#creerChapitre'
-            });
-        </script>
+        <script>tinymce.init({
+                selector:'.mytextarea',
+                width: 1000,
+                height: 500,
+                plugins: "advlist autolink link image lists charmap print preview hr anchor pagebreak spellchecker " +
+                "searchreplace wordcount visualblocks visualchars code fullscreen insertdatetime media nonbreaking " +
+                "save table contextmenu directionality emoticons template paste textcolor autosave",
+                toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright " +
+                "alignjustify | bullist numlist outdent indent | link image | print preview media fullpage | " +
+                "forecolor backcolor emoticons | restoredraft spellchecker"
+            });</script>
+
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+        <!-- Optional theme -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+        <!-- Latest compiled and minified JavaScript -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
         <title><?= $titre ?></title>
     </head>
     <body>
