287fa40f5aaec9ee3f44576ea3c0b0489e9a55d8
pywako <wongaliceyy@gmail.com>
2017-06-11 22:41:32 +0200

    Nettoyage et séparation création / modification dans un autre fichier vue

--- Vue/Admin/creation.php
+++ Vue/Admin/creation.php
@@ -2,8 +2,7 @@
 /**
 Page d'écriture et modification du chapitre
  */
-if (empty($chapitre)) // Ecriture
-{
+
     $this->titre = "Billet pour l'Alaska - Ecriture";
     ?>
     <header>
@@ -22,26 +21,3 @@ if (empty($chapitre)) // Ecriture
     </textarea>
         <input type="submit" value = "envoyer">
     </form>
-
-<?php}
-else // Modification
-{
-    $this->titre = "Billet pour l'Alaska - Modifier";
-    ?>
-    <header>
-        <h2>Modification d'un chapitre</h2>
-    </header>
-    <form method="post" action="admin/modifierChapitre">
-        <p>
-            <label for="titre">Titre du chapitre : </label>
-            <input type="text" name="titre" id="titre" value="<?= $this->nettoyer($chapitre['titre'])?>" required> <br>
-            <label for="numero">Chapitre n° : </label>
-            <input type="text" name="numero" id="numero" value="<?php echo $this->nettoyer($chapitre['numero'])?>" required> <br>
-        </p>
-        <textarea id="creerChapitre" name="contenu" value="<?= $this->nettoyer($chapitre['contenu'])?>" required>
-        </textarea>
-        <input type="hidden" name="id" id="id" value="<?php echo $this->nettoyer($chapitre['numero'])?>">
-        <input type="submit" value = "envoyer">
-    </form>
-
-<?php } ?>
