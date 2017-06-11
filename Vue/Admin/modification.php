7031e56bd739929a48fe11604aae82bd97401e15
pywako <wongaliceyy@gmail.com>
2017-06-11 22:42:27 +0200

    ajout du fichier vue modification avec le préremplissage des champs en vue de la modification

---
+++ Vue/Admin/modification.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Vue modification d'un chapitre
+ */
+$this->titre = "Billet pour l'Alaska - Modifier";
+?>
+<header>
+    <h2>Modification d'un chapitre</h2>
+</header>
+<form method="post" action="admin/modifierChapitre">
+    <p>
+        <label for="titre">Titre du chapitre : </label>
+        <input type="text" name="titre" id="titre" value="<?php echo $this->nettoyer($chapitre['titre'])?>" required> <br>
+        <label for="numero">Chapitre n° : </label>
+        <input type="text" name="numero" id="numero" value="<?php echo $this->nettoyer($chapitre['numero'])?>" required> <br>
+        <input type="hidden" name="id" id="id" value="<?php echo $chapitre['id']?>">
+    </p>
+    <textarea id="contenuChapitre" name="contenuChapitre" required>
+        <?php echo $this->nettoyer($chapitre['contenu'])?>
+        </textarea>
+
+    <input type="submit" value = "envoyer">
+</form>
