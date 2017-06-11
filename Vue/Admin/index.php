aa96f896f7e23e1c42322e2c01d7d55d29765084
pywako <wongaliceyy@gmail.com>
2017-06-11 17:55:25 +0200

    Ajout du tableau de gestion des chapitres

--- Vue/Admin/index.php
+++ Vue/Admin/index.php
@@ -15,3 +15,24 @@ Ce blog comporte <?= $this->nettoyer($nbChapitres) ?> chapitre(s)
  et <?= $this->nettoyer($nbCommentaires) ?> commentaire(s).
 <a id="lienDeco" href="connexion/deconnecter">Se déconnecter</a><br>
 <a id="ecrire" href="admin/creation"><button>Ecrire un chapitre</button></a>
+
+<h3>Liste des chapitres</h3>
+<table>
+    <tr>
+        <th>Chapitre</th>
+        <th>Titre</th>
+        <th>Date</th>
+        <th>Nombre de commentaire</th>
+        <th>Actions</th>
+    </tr>
+    <?php foreach ($chapitres as $chapitre):?>
+        <tr>
+            <td><?= $chapitre['numero'] ?></td>
+            <td><?= $this->nettoyer($chapitre['titre']) ?></td>
+            <td><?= $this->nettoyer($chapitre['date']) ?></td>
+            <td><?= $this->nettoyer($chapitre['nbcom']) ?></td>
+            <td><a id="supprimerChapitre" href="<?= "admin/supprimerChapitre/" . $chapitre['id']."/". $chapitre['numero']?>"><button>Supprimer un chapitre</button></a><br>
+                <a id="modifierChapitre" href="<?= "admin/ModifierChapitre/" . $chapitre['id']."/". $chapitre['numero']?>?>"><button>Modifier un chapitre</button></a></td>
+        </tr>
+    <?php endforeach; ?>
+</table>
