d485206f48dd062e3759b0275121eaca91d32789
pywako <wongaliceyy@gmail.com>
2017-06-24 16:35:14 +0200

    Remaniement et ajout de contrôle pour l'action commenter dans le fichier controleurChapitre

--- Controleur/ControleurChapitre.php
+++ Controleur/ControleurChapitre.php
@@ -16,7 +16,7 @@ class ControleurChapitre extends Controleur {
      * Constructeur 
      */
     public function __construct() {
-        $this->chapitre = new Chapitre();
+        $this->chapitre    = new Chapitre();
         $this->commentaire = new Commentaire();
     }
 
@@ -24,31 +24,57 @@ class ControleurChapitre extends Controleur {
     public function index() {
         if(isset($_SESSION['id']))
         {
-            $idChapitre = $this->requete->getSession()->getAttribut("id");
+            $idChapitre     = $this->requete->getSession()->getAttribut("id");
+            $chapitre       = $this->chapitre->getChapitre($idChapitre);
+            $commentaires   = $this->commentaire->getCommentaires($idChapitre);
         }
-        else
+        elseif (isset($_GET['id']))
         {
-            $idChapitre = $this->requete->getParametre("id");
+            $idChapitre     = $this->requete->getParametre("id");
+            $chapitre       = $this->chapitre->getChapitre($idChapitre);
+            $commentaires   = $this->commentaire->getCommentaires($idChapitre);
         }
-        $chapitre = $this->chapitre->getChapitre($idChapitre);
-        $commentaires = $this->commentaire->getCommentaires($idChapitre);
+        else{
+            $this->requete->getSession()->setflash("Le chapitre sélectionner n'existe pas :(", "danger");
+        }
         $session = $this->requete->getSession();
-        $this->genererVue(array('chapitre' => $chapitre, 'commentaires' => $commentaires, 'session' => $session));
+        $this->genererVue(array(
+            'chapitre' => $chapitre,
+            'commentaires' => $commentaires,
+            'session' => $session));
     }
 
     // Ajoute un commentaire sur un billet
     public function commenter() {
-        $idChapitre = $this->requete->getParametre("id");
-        $auteur = htmlspecialchars($this->requete->getParametre("auteur"));
-        $contenu = htmlspecialchars($this->requete->getParametre("contenu"));
-        $this->commentaire->ajouterCommentaire($auteur, $contenu, $idChapitre);
-        $this->requete->getSession()->setflash("Commentaire ajouté !", "success");
+        if(isset($_POST['auteur']) && !empty($_POST['auteur']))
+        {
+            $auteur = htmlspecialchars($this->requete->getParametre("auteur"));
+            if(isset($_POST['contenu']) && !empty($_POST['contenu']))
+            {
+                $contenu = htmlspecialchars($this->requete->getParametre("contenu"));
+                if(isset($_POST['id']) && !empty($_POST['id']))
+                {
+                    $idChapitre = $this->requete->getParametre("id");
+                    $this->commentaire->ajouterCommentaire($auteur, $contenu, $idChapitre);
+                    $this->requete->getSession()->setflash("Merci pour votre retour, Commentaire ajouté ! ;)", "success");
+                }
+                else{
+                    $this->requete->getSession()->setflash("id du chapitre non reconnu :(", "danger");
+                }
+            }
+            else{
+                $this->requete->getSession()->setflash("Vous n'avez rien écrit :(", "danger");
+            }
+        }
+        else{
+            $this->requete->getSession()->setflash("comment vous appelez vous?", "danger");
+        }
         // Exécution de l'action par défaut pour réafficher la liste des billets
         $this->executerAction("index");
     }
     public function signaler() {
         $commentaireId = $this->requete->getParametre("id");
-        $commentaire = $this->commentaire->getOneCommentaire($commentaireId);
+        $commentaire   = $this->commentaire->getOneCommentaire($commentaireId);
         $this->commentaire->signaler($commentaireId);
         $this->requete->getSession()->setflash("Le commentaire a bien été signalé.", "success");
         // Exécution de l'action par défaut pour réafficher la liste des billets
