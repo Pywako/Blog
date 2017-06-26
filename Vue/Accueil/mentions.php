14554c89daadb45a15ce315c956dd7fc96ed7862
pywako <wongaliceyy@gmail.com>
2017-06-26 16:03:15 +0200

    ajout de texte mentions légales

--- Vue/Accueil/mentions.php
+++ Vue/Accueil/mentions.php
@@ -6,6 +6,10 @@ Mentions légales du site / Licence / droits d'auteurs
 <article>
     <h2>Mentions légales</h2>
     <p>
-        Mentions légales
+        Sed vel laoreet est. Quisque tincidunt dignissim ante vitae sodales. Morbi feugiat blandit lacus, at laoreet lacus. Nunc et quam posuere, ullamcorper elit a, efficitur arcu. Vivamus ut semper augue. Phasellus scelerisque metus quis risus posuere dictum. Donec elementum eleifend purus venenatis mollis. Fusce felis purus, dapibus at maximus non, volutpat a tellus. Curabitur ac purus lectus. Morbi at augue ut tortor pharetra volutpat tristique nec est. Suspendisse eleifend velit ut est malesuada malesuada. Pellentesque nec augue pulvinar, scelerisque justo eu, mollis erat. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Cras facilisis, nisl id finibus dignissim, quam lorem commodo diam, vel ultricies enim metus ac augue.
+
+        Fusce porttitor quam at nisl pharetra, condimentum ultrices lacus auctor. Phasellus nec sapien justo. Pellentesque ac rhoncus erat. Nam at pretium est. Aliquam non congue leo, eu gravida est. Aenean sed semper ex. Integer scelerisque porta sapien vitae imperdiet. Nullam hendrerit nulla ac porttitor porttitor.
+
+        Aenean in arcu accumsan, tristique purus vitae, blandit orci. Quisque viverra, sapien ac dapibus sagittis, dui magna vehicula libero, at dapibus purus nisl quis quam. Vivamus non justo vel dui hendrerit rhoncus. Ut sit amet elit erat. Ut sagittis dictum sapien, eu mollis nunc egestas eget. Etiam quis placerat ex. Aliquam et odio at nulla commodo mollis sed at quam. Etiam et nulla id quam molestie blandit.
     </p>
 </article>
