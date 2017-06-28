98bba29b29cc71b72a5196c2464254d326235983
pywako <wongaliceyy@gmail.com>
2017-06-28 06:49:12 +0200

    Ajout test forme des login et mdp connexion

--- Controleur/ControleurConnexion.php
+++ Controleur/ControleurConnexion.php
@@ -31,22 +31,39 @@ class ControleurConnexion extends Controleur
         {
             $login = $this->requete->getParametre("login");
             $mdp = $this->requete->getParametre("mdp");
-            if($this->utilisateur->connecter($login, $mdp))
+            $patternLogin = "#[a-zA-Z0-9]{4}#";
+            if(preg_match($patternLogin, $login)){
+                $patternMdp = "#[a-zA-Z0-9]{4}#";
+                if (preg_match($patternMdp, $mdp)){
+                    if($this->utilisateur->connecter($login, $mdp))
+                    {
+                        $utilisateur = $this->utilisateur->getUtilisateur($login);
+                        $this->requete->getSession()->setAttribut("idUtilisateur", $utilisateur['idUtilisateur']);
+                        $this->requete->getSession()->setAttribut("login", $utilisateur['login']);
+                        $this->requete->getSession()->setAttribut("page", 1);
+                        $this->rediriger("Admin");
+                    }
+                    else
+                    {
+                        $this->requete->getSession()->setflash("Login ou mot de passe incorrects");
+                        $this->rediriger("connexion");
+                    }
+                }else
+                {
+                    $this->requete->getSession()->setflash("Mot de passe non conforme");
+                    $this->rediriger("connexion");
+                }
+            }else
             {
-                $utilisateur = $this->utilisateur->getUtilisateur($login);
-                $this->requete->getSession()->setAttribut("idUtilisateur", $utilisateur['idUtilisateur']);
-                $this->requete->getSession()->setAttribut("login", $utilisateur['login']);
-                $this->requete->getSession()->setAttribut("page", 1);
-                $this->rediriger("Admin");
-            }
-            else
-            {
-                $this->requete->getSession()->setflash("Login ou mot de passe incorrects");
+                $this->requete->getSession()->setflash("Login non conforme");
                 $this->rediriger("connexion");
             }
         }
         else
-            throw new \Exception ("Action impossible : login ou mot de passe non défini");
+        {
+            $this->requete->getSession()->setflash("Action impossible : login ou mot de passe non défini");
+            $this->rediriger("connexion");
+        }
     }
 
     /**
